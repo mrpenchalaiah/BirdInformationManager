6c22c17311a9a945662d06dc3cc7aec566452c7d
mpenchalaiah <mpenchalaiah@travelguru.com>
2017-11-23 14:37:28 +0530

    added swagger implementation with mongo db

--- src/main/java/se/saltside/controllers/BirdAPIController.java
+++ src/main/java/se/saltside/controllers/BirdAPIController.java
@@ -1,12 +1,15 @@
 package se.saltside.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import java.util.HashSet;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,7 +29,8 @@ import se.saltside.processor.BirdInformationRetriver;
 import se.saltside.processor.BirdResponseConverter;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/*")
+@Api(value="/", description="birds information store and retrival")
 public class BirdAPIController {
 
 	@Autowired
@@ -34,21 +38,42 @@ public class BirdAPIController {
 	@Autowired
 	private BirdResponseConverter birdResponseconverter;
 	
+	
+	@ApiOperation(value = "View a available bird stored", response = BirdListResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
 	@RequestMapping(path="/birds",method=RequestMethod.GET)
 	public BirdListResponse getAllBirds() {
 		List<Bird> birds = birdRetriver.getAllBirds();
-		if(null== birds || birds.size()==0) {
-			 throw new BirdNotFoundException("The bird you deleting was not found");
-		 }
 		return birdResponseconverter.convertToBirdResponse(new HashSet<Bird>(birds));
 	}
 	
+	
+	@ApiOperation(value = "store the bird information in database", response = HttpStatus.class)
+	 @ApiResponses(value = {
+	            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+	            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+	            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+	            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	    })
 	@RequestMapping(path="/birds",method=RequestMethod.POST)
 	public ResponseEntity<Void> saveBird(@Valid @RequestBody BirdRequest bird) {
 		birdRetriver.storeBird(birdResponseconverter.getBird(bird));
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
+
 	
+	@ApiOperation(value = "delete the bird information from database based on id", response = HttpStatus.class)
+	 @ApiResponses(value = {
+	            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+	            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+	            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+	            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	    })
 	@RequestMapping(path="/birds/{birdid}",method=RequestMethod.DELETE)
 	public ResponseEntity<Void>  deleteBird(@PathVariable("birdid") String id) {
 		Bird bird = birdRetriver.getBird(id);
@@ -59,6 +84,13 @@ public class BirdAPIController {
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "retrive the bird information from database based on id", response = BirdDetailResponse.class)
+	 @ApiResponses(value = {
+	            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+	            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+	            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+	            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	    })
 	@RequestMapping(path="/birds/{birdid}",method=RequestMethod.GET)
 	public BirdDetailResponse getBird(@PathVariable("birdid") String birdid) {
 		 Bird bird = birdRetriver.getBird(birdid);
